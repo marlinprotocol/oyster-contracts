2da1fa7b970593dbcc7d911bd1295085c1c6c78d
Vikrant Garg <vikrant1garg@gmail.com>
2024-05-31 15:11:14 +0530

    Update attestation generation utility

--- attestation.js
+++ attestation.js
@@ -7,10 +7,11 @@ const image = {
 };
 
 async function createAttestation(
-	enclaveKey,
-    verifierEnclavePrivateKey,
-	timestamp,
+	enclaveKey
 ) {
+    verifierEnclavePrivateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+    time = new Date();
+    timestamp = time.getTime();
 	const domain = {
 		name: 'marlin.oyster.AttestationVerifier',
 		version: '1',
