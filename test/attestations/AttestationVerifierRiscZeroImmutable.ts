5fd4f8b918e6fd858a44318ab9286b5d4bfdb9d2
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-10-25 18:30:32 +0530

    add verify tests, simply check if it forwards correctly

--- test/attestations/AttestationVerifierRiscZeroImmutable.ts
+++ test/attestations/AttestationVerifierRiscZeroImmutable.ts
@@ -24,3 +24,41 @@ describe("AttestationVerifierRiscZeroImmutable - Init", function() {
     expect(await contract.IMAGE_ID()).to.equal(id("some image"));
   });
 });
+
+describe("AttestationVerifierRiscZeroImmutable - Verify", function() {
+  let signers: Signer[];
+  let addrs: string[];
+
+  let contract: AttestationVerifierRiscZeroImmutable;
+
+  before(async function() {
+    signers = await ethers.getSigners();
+    addrs = await Promise.all(signers.map((a) => a.getAddress()));
+
+    const Contract = await ethers.getContractFactory("AttestationVerifierRiscZeroImmutable");
+    contract = (await Contract.deploy(id("some image"))) as unknown as AttestationVerifierRiscZeroImmutable;
+
+    expect(await contract.IMAGE_ID()).to.equal(id("some image"));
+  });
+
+  takeSnapshotBeforeAndAfterEveryTest(async () => { });
+
+  it("verifies", async function() {
+    const Mock = await ethers.getContractFactory("MockRiscZeroVerifier");
+    let mock = await Mock.deploy();
+    await mock.setFail(false);
+    await mock.setExpectedCall(id("some seal"), id("some image"), id("some journal"));
+
+    await expect(contract.verify(await mock.getAddress(), id("some image"), id("some journal"), id("some seal")))
+      .to.not.be.reverted;
+  });
+
+  it("reverts if verifier reverts", async function() {
+    const Mock = await ethers.getContractFactory("MockRiscZeroVerifier");
+    let mock = await Mock.deploy();
+    await mock.setFail(true);
+
+    await expect(contract.verify(await mock.getAddress(), id("some image"), id("some journal"), id("some seal")))
+      .to.be.revertedWith("verification failed");
+  });
+});
