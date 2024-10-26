52c045734c16c627fd74ccda4278a6b3c498e521
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-10-26 18:12:05 +0530

    rename the interface and redo natspec, it is no longer attestation specific

---
+++ contracts/attestations/IOysterVerifierRiscZero.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./IRiscZeroVerifier.sol";
+
+/// @title IOysterVerifierRiscZero
+/// @notice Interface for verifying proofs generated using a specific RiscZero guest image
+interface IOysterVerifierRiscZero {
+    /// @notice Error thrown when an unknown image is encountered
+    error OysterVerifierRiscZeroUnknownImage();
+
+    /// @notice Verifies a proof generated using a specific RiscZero guest image
+    /// @param _imageId The RiscZero guest image id
+    /// @param _verifier The RiscZero verifier contract
+    /// @param _journalDigest The journal digest
+    /// @param _seal The seal data
+    function verify(
+        bytes32 _imageId,
+        IRiscZeroVerifier _verifier,
+        bytes32 _journalDigest,
+        bytes calldata _seal
+    ) external view;
+
+    /// @notice Verifies a proof generated using a specific RiscZero guest image
+    /// @param _data ABI encoded parameters
+    function verify(bytes calldata _data) external view;
+}
