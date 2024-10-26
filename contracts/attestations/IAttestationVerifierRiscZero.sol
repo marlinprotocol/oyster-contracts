2d562bd03b0966cc4230b875eb9b7cf4e95fced9
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-10-26 17:31:25 +0530

    add natspec

--- contracts/attestations/IAttestationVerifierRiscZero.sol
+++ contracts/attestations/IAttestationVerifierRiscZero.sol
@@ -4,9 +4,17 @@ pragma solidity ^0.8.0;
 
 import "./IRiscZeroVerifier.sol";
 
+/// @title IAttestationVerifierRiscZero
+/// @notice Interface for verifying attestations using RISC Zero
 interface IAttestationVerifierRiscZero {
+    /// @notice Error thrown when an unknown image is encountered
     error AttestationVerifierRiscZeroUnknownImage();
 
+    /// @notice Verifies an attestation using RISC Zero
+    /// @param _imageId The ID of the image to verify
+    /// @param _verifier The RISC Zero verifier contract
+    /// @param _journalDigest The digest of the journal
+    /// @param _seal The seal data
     function verify(
         bytes32 _imageId,
         IRiscZeroVerifier _verifier,
@@ -14,5 +22,7 @@ interface IAttestationVerifierRiscZero {
         bytes calldata _seal
     ) external view;
 
+    /// @notice Verifies an attestation using encoded data
+    /// @param _data The encoded attestation data
     function verify(bytes calldata _data) external view;
 }
