0c456bb15b351754f1fb9ebc61435da13e37cc96
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-10-27 12:27:18 +0530

    simply foward to the underlying verifier for now, yet to implement stop

--- contracts/attestations/OysterVerifierRiscZeroStoppable.sol
+++ contracts/attestations/OysterVerifierRiscZeroStoppable.sol
@@ -9,29 +9,12 @@ import "./IOysterVerifierRiscZero.sol";
 /// The contract is immutable.
 contract OysterVerifierRiscZeroStoppable is IOysterVerifierRiscZero {
     /// @notice The RiscZero guest image id
-    bytes32 public immutable IMAGE_ID;
+    IOysterVerifierRiscZero public immutable VERIFIER;
 
     /// @notice Constructs the OysterVerifierRiscZeroStoppable contract
     /// @param _imageId The RiscZero guest image id
-    constructor(bytes32 _imageId) {
-        IMAGE_ID = _imageId;
-    }
-
-    /// @notice Internal function to foward verification calls to the verifier
-    /// @param _imageId The RiscZero guest image id
-    /// @param _verifier The RiscZero verifier contract
-    /// @param _journalDigest The journal digest
-    /// @param _seal The seal data
-    /// @dev Reverts if the image id does not match IMAGE_ID
-    function _verify(
-        bytes32 _imageId,
-        IRiscZeroVerifier _verifier,
-        bytes32 _journalDigest,
-        bytes memory _seal
-    ) internal view {
-        require(_imageId == IMAGE_ID, OysterVerifierRiscZeroUnknownImage());
-
-        _verifier.verify(_seal, _imageId, _journalDigest);
+    constructor(IOysterVerifierRiscZero _verifier) {
+        VERIFIER = _verifier;
     }
 
     /// @notice Verifies a proof generated using a specific RiscZero guest image
@@ -45,16 +28,12 @@ contract OysterVerifierRiscZeroStoppable is IOysterVerifierRiscZero {
         bytes32 _journalDigest,
         bytes calldata _seal
     ) external view {
-        _verify(_imageId, _verifier, _journalDigest, _seal);
+        VERIFIER.verify(_imageId, _verifier, _journalDigest, _seal);
     }
 
     /// @notice Verifies a proof generated using a specific RiscZero guest image
     /// @param _data ABI encoded parameters
     function verify(bytes calldata _data) external view {
-        (bytes32 _imageId, IRiscZeroVerifier _verifier, bytes32 _journalDigest, bytes memory _seal) = abi.decode(
-            _data,
-            (bytes32, IRiscZeroVerifier, bytes32, bytes)
-        );
-        _verify(_imageId, _verifier, _journalDigest, _seal);
+        VERIFIER.verify(_data);
     }
 }
