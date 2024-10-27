13b850fdb441422bbb4267103ef994e89ab837a7
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-10-27 19:35:34 +0530

    add tests for the unpause function

--- test/attestations/OysterVerifierRiscZeroStoppable.ts
+++ test/attestations/OysterVerifierRiscZeroStoppable.ts
@@ -83,6 +83,55 @@ describe("OysterVerifierRiscZeroStoppable - Pause", function() {
   });
 });
 
+describe("OysterVerifierRiscZeroStoppable - Unpause", function() {
+  let signers: Signer[];
+  let addrs: string[];
+
+  let contract: OysterVerifierRiscZeroStoppable;
+
+  before(async function() {
+    signers = await ethers.getSigners();
+    addrs = await Promise.all(signers.map((a) => a.getAddress()));
+
+    const Contract = await ethers.getContractFactory("OysterVerifierRiscZeroStoppable");
+    contract = (await Contract.deploy(addrs[10], addrs[1])) as unknown as OysterVerifierRiscZeroStoppable;
+
+    expect(await contract.VERIFIER()).to.equal(addrs[10]);
+    expect(await contract.owner()).to.equal(addrs[1]);
+    expect(await contract.paused()).to.equal(false);
+  });
+
+  takeSnapshotBeforeAndAfterEveryTest(async () => { });
+
+  it("admin can unpause", async function() {
+    await contract.connect(signers[1]).pause();
+    expect(await contract.paused()).to.equal(true);
+
+    await contract.connect(signers[1]).unpause();
+
+    expect(await contract.paused()).to.equal(false);
+  });
+
+  it("admin cannot unpause when never paused", async function() {
+    await expect(contract.connect(signers[1]).unpause()).to.be.revertedWithCustomError(contract, "ExpectedPause");
+  });
+
+  it("admin cannot unpause when unpaused", async function() {
+    await contract.connect(signers[1]).pause();
+    await contract.connect(signers[1]).unpause();
+    expect(await contract.paused()).to.equal(false);
+
+    await expect(contract.connect(signers[1]).unpause()).to.be.revertedWithCustomError(contract, "ExpectedPause");
+  });
+
+  it("non admin cannot unpause", async function() {
+    await contract.connect(signers[1]).pause();
+    expect(await contract.paused()).to.equal(true);
+
+    await expect(contract.unpause()).to.be.revertedWithCustomError(contract, "OwnableUnauthorizedAccount");
+  });
+});
+
 describe("OysterVerifierRiscZeroStoppable - Verify", function() {
   let signers: Signer[];
   let addrs: string[];
